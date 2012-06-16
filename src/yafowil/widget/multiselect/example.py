eca75aeb266070f2706ba2402f650bbf4a7241a4
Johannes Raggam <raggam-nl@adm.at>
2012-06-16 14:42:09 +0200

    sort example vocab, remove defaults from selection widget

--- src/yafowil/widget/multiselect/example.py
+++ src/yafowil/widget/multiselect/example.py
@@ -6,7 +6,7 @@ def get_example():
     part = factory(u'fieldset', name='yafowilwidgetmultiselect')
     part['text'] = factory('field:label:error:multiselect', props={
         'label': 'Enter some text (local, lorem ipsum)',
-        'vocabulary': (u'Weißburgunder', u'Welschriesling',
+        'vocabulary': sorted((u'Weißburgunder', u'Welschriesling',
                        u'Sauvingnon Blanc', u'Sämling', u'Scheurebe',
-                       u'Traminer', u'Morrilon', u'Muskateller')})
+                       u'Traminer', u'Morrilon', u'Muskateller'))})
     return [{'widget': part, 'doc': 'TODO'}]
--- src/yafowil/widget/multiselect/widget.py
+++ src/yafowil/widget/multiselect/widget.py
@@ -24,28 +24,3 @@ factory.defaults['multiselect.size'] = None
 factory.defaults['multiselect.default'] = []
 factory.defaults['multiselect.format'] = 'block'
 factory.defaults['multiselect.class'] = 'multiselect'
-
-factory.defaults['multiselect.listing_tag'] = 'div'
-factory.doc['props']['multiselect.listing_tag'] = """\
-Desired rendering tag for selection if selection format is 'single'. Valid
-values are 'div' and 'ul'.
-"""
-
-factory.defaults['multiselect.listing_label_position'] = 'inner-after'
-factory.doc['props']['multiselect.listing_label_position'] = """\
-Label position if format is 'single'. Behaves the same way as label widget
-position property.
-"""
-
-factory.doc['props']['multiselect.vocabulary'] = """\
-Vocabulary to be used for the selection list. Expects a dict-like or an
-iterable or a callable which returns one of both first. An iterable can consist
-out of unicode objects or out of tuples with ``(key, value)``. Use unicode
-instead of strings.
-"""
-
-factory.doc['props']['multiselect.disabled'] = """\
-Disables the whole widget or single selections. To disable the whole widget
-set the value to 'True'. To disable single selection pass a iterable of keys to
-disable, i.e. ``['foo', 'baz']``. Defaults to False.
-"""
