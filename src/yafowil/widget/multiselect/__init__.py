ff57c430538aecdce736e570f2e76febab63ef94
Robert Niederreiter <office@squarewave.at>
2012-06-25 17:22:51 +0200

    resources

--- src/yafowil/widget/multiselect/__init__.py
+++ src/yafowil/widget/multiselect/__init__.py
@@ -1,31 +1,47 @@
 import os
+from yafowil.base import factory
+
+
+resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+
+js = [{
+    'resource': 'multi-select/js/jquery.multi-select.js',
+    'thirdparty': False,
+    'order': 20,
+}, {
+    'resource': 'widget.js',
+    'thirdparty': False,
+    'order': 21,
+}]
+
+css = [{
+    'resource': 'multi-select/css/multi-select.css',
+    'thirdparty': False,
+    'order': 20,
+}, {
+    'resource': 'widget.css',
+    'thirdparty': False,
+    'order': 21,
+}]
 
 
 def register():
     import widget
+    factory.register_theme('default', 'yafowil.widget.multiselect',
+                           resourcedir, js=js, css=css)
 
+
+###############################################################################
+# XXX: outdated below
+###############################################################################
 
 def get_resource_dir():
-    return os.path.join(os.path.dirname(__file__), 'resources')
+    return resourcedir
 
 
 def get_js():
-    return [
-        {'resource': 'multi-select/js/jquery.multi-select.js',
-         'thirdparty': False,
-         'order': 20,},
-        {'resource': 'widget.js',
-         'thirdparty': False,
-         'order': 21,}
-    ]
+    return js
 
 
 def get_css():
-    return [
-        {'resource': 'multi-select/css/multi-select.css',
-         'thirdparty': False,
-         'order': 20,},
-        {'resource': 'widget.css',
-         'thirdparty': False,
-         'order': 21,},
-    ]
+    return css
