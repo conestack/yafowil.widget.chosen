0773c45d001765791f4b82211d0a84a10aa20569
Robert Niederreiter <office@squarewave.at>
2018-02-01 22:38:33 +0100

    Use yafowil.tests.YafowilTestCase

--- src/yafowil/widget/chosen/tests.py
+++ src/yafowil/widget/chosen/tests.py
@@ -1,13 +1,17 @@
-from node.tests import NodeTestCase
 from node.utils import UNSET
 from yafowil.base import ExtractionError
 from yafowil.base import factory
+from yafowil.tests import YafowilTestCase
 from yafowil.tests import fxml
-import yafowil.widget.chosen
 import yafowil.loader
 
 
-class TestChosenWidget(NodeTestCase):
+class TestChosenWidget(YafowilTestCase):
+
+    def setUp(self):
+        super(TestChosenWidget, self).setUp()
+        from yafowil.widget.chosen import widget
+        reload(widget)
 
     def test_edit_renderer(self):
         widget = factory(
