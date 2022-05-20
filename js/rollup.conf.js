2deb91ad1bab4223126977d1bf4504bf3f25ec47
Robert Niederreiter <rnix@squarewave.at>
2022-05-20 13:30:46 +0200

    "Simplify IIFE bundle outro"

--- js/rollup.conf.js
+++ js/rollup.conf.js
@@ -4,9 +4,7 @@ import {terser} from 'rollup-plugin-terser';
 const out_dir = 'src/yafowil/widget/chosen/resources';
 
 const outro = `
-if (window.yafowil === undefined) {
-    window.yafowil = {};
-}
+window.yafowil = window.yafowil || {};
 window.yafowil.chosen = exports;
 `;
 
