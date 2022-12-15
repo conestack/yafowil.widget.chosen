43d09b82b57662954d3681611968120d58fa29e9
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2022-12-15 10:17:52 +0100

    update test

--- js/tests/test_chosen.js
+++ js/tests/test_chosen.js
@@ -1,5 +1,7 @@
 import { ChosenWidget } from "../src/widget";
 
+window.yafowil_array = undefined;
+
 QUnit.test('test', assert => {
     let el = $('<select class="chosen" />').appendTo('body');
     let wid = new ChosenWidget(el);
